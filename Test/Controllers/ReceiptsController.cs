d3b8b5f38c9feba1a7f90d5c1ae06ca626d75152
sergey19941201 <koshelnick1994@mail.ru>
2017-04-03 13:52:52 +0300

    ing

--- Test/Controllers/ReceiptsController.cs
+++ Test/Controllers/ReceiptsController.cs
@@ -77,7 +77,7 @@ namespace Test.Controllers
             return View(receipts);
         }
 
-        // POST: Receipts/Edit/5
+        // POST: Receipts/Edit/5/
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
